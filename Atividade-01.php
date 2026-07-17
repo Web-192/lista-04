994ebcd68451ab874392b907ebe15717b8f57d42
Caua <cauasantiagofreitas23241@gmail.com>
2026-07-16 21:59:04 -0300

    Atividade de hoje

---
+++ Atividade-01.php
@@ -0,0 +1,35 @@
+<?php
+// Entradas
+$nome = " maria aparecida de souza ";
+echo ("Ex. trim:" . trim($nome));
+echo ("Ex. ucwords:" . ucwords($nome));
+echo ("Ex: strlen:" . strlen($nome));
+
+$curso = "programação web";
+
+$nota = 8.756;
+echo ("Ex:number_format(): " . number_format($numero = 8.756));
+echo ("<br>");
+
+echo ("Ex: round(): " . round($nota));
+echo ("<br>");
+
+echo ("Ex: ceil(): " . ceil($nota));
+echo ("<br>");
+
+echo ("Ex: floor(): " . floor($nota));
+echo ("<br>")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web</title>
+</head>
+<body>
+    <h3>Atividade 1</h3>
+    <p><?php  ?></p>
+</body>
+</html>
---
+++ Atividade-02.php
@@ -0,0 +1,11 @@
+<?php
+$produto = "Notebook Gamer";
+echo ("Ex. strtoupper:" . strtoupper($produto));
+echo ("<br>");
+echo ("Ex. str_replace():" . str_replace("Gamer", "Positivo", $produto));
+echo ("<br>");
+$preco = 4599.989;
+echo ("strtolower:" . strtolower($preco));
+echo ("<br>");
+$desconto = -350;
+?>
